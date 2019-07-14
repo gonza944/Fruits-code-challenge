cc579248849bd24a3375293ae2aed67f88e5b3b2
Gon Ariza <gonza94.4@gmail.com>
2019-07-14 14:58:21 -0300

    implemented angular icons for the up/down icon

--- src/app/fruits-module/fruits-module.module.ts
+++ src/app/fruits-module/fruits-module.module.ts
@@ -2,11 +2,14 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FruitsComponent } from './fruits/fruits.component';
 import { MatButtonModule } from '@angular/material';
+import {MatIconModule} from '@angular/material/icon';
 
 @NgModule({
   declarations: [FruitsComponent],
   imports: [
     CommonModule,
+    MatButtonModule,
+    MatIconModule,
   ],
   exports: [FruitsComponent],
   entryComponents: [FruitsComponent]
